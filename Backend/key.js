4b6e6f1fbade8a7ff75bceb142379d6da223fda2
Saumya Singh <115228860+SaumyaSingh1@users.noreply.github.com>
2023-08-21 17:22:28 +0530

    Update key.js

--- Backend/key.js
+++ Backend/key.js
@@ -1,3 +1,1 @@
-export const mongoUrl = "mongodb+srv://friendvibe:friendvibe123@cluster0.h1gi0bm.mongodb.net/socialmediasite?retryWrites=true&w=majority"
 
-export const Jwt_secret = "thekeysecret";
